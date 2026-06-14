243bfd1e258a65e3d276d3ab74f2ba82b00ff920
Sadiq Amin <engrsadiq13@gmail.com>
2026-06-14 20:54:33 +0500

    members information added

--- library_app/models.py
+++ library_app/models.py
@@ -1,10 +1,20 @@
 from django.db import models
 
+
 class Book(models.Model):
     isbn = models.CharField(max_length=20, unique=True)
     title = models.CharField(max_length=255)
     author = models.CharField(max_length=255)
-    copies = models.PositiveIntegerField()
+    copies = models.PositiveIntegerField(default=1)
 
     def __str__(self):
         return self.title
+
+
+class Member(models.Model):
+    member_id = models.PositiveIntegerField(unique=True)
+    name = models.CharField(max_length=100)
+    borrowed_books = models.ManyToManyField(Book, blank=True)
+
+    def __str__(self):
+        return self.name
--- library_app/views.py
+++ library_app/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
-from .models import Book
+from django.shortcuts import render, get_object_or_404
+from .models import Book, Member
+
 
 def book_list(request):
     books = Book.objects.all()
@@ -9,5 +10,32 @@ def book_list(request):
         'books/list.html',
         {
             'books': books
+        }
+    )
+
+
+def member_list(request):
+    members = Member.objects.all()
+
+    return render(
+        request,
+        'library_app/member_list.html',
+        {
+            'members': members
+        }
+    )
+
+
+def member_detail(request, member_id):
+    member = get_object_or_404(
+        Member,
+        member_id=member_id
+    )
+
+    return render(
+        request,
+        'library_app/member_detail.html',
+        {
+            'member': member
         }
     )
